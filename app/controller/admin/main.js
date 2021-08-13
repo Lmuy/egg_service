eebd976fbab7f729104c27560c22eb4500cc67c5
liukangning <liukangning@kingsoft.com>
2021-08-13 16:29:26 +0800

    删除和修改接口

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -50,6 +50,43 @@ class MainController extends Controller {
       isSuccess: updateSuccess
     }
   }
+  // 获取文章列表
+  async getArticleList() {
+    let sql = 'SELECT article.id as id,'+
+      'article.title as title,'+
+      'article.introduce as introduce,'+
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
+      'type.typeName as typeName '+
+      'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+      'ORDER BY article.id DESC '
+
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = {list: result}
+  }
+  // 删除文章
+  async delArticle() {
+    let id = this.ctx.params.id
+    const res = await this.app.mysql.delete('article', {'id': id})
+
+    this.ctx.body = {data: res}
+  }
+  // 文章详情
+  async getArticleById() {
+    let id = this.ctx.params.id
+
+    let sql = 'SELECT article.id as id,'+
+      'article.title as title,'+
+      'article.introduce as introduce,'+
+      'article.article_content as article_content,'+
+      "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
+      'article.view_count as view_count ,'+
+      'type.typeName as typeName ,'+
+      'type.id as typeId '+
+      'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+      'WHERE article.id='+id
+    const result = await this.app.mysql.query(sql)
+    this.ctx.body = {data: result}
+  }
 }
 
 module.exports = MainController
--- app/middleware/adminauth.js
+++ app/middleware/adminauth.js
@@ -1,7 +1,6 @@
 module.exports = options => {
 
   return async function adminauth(ctx, next) {
-    console.log (ctx.session)
     if (ctx.session.openId) {
       await next()
     } else {
